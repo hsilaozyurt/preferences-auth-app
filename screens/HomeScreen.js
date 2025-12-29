dbe674650f1729b31aedf853116a18ec53edda7c
Sıla Özyurt <hso1129@icloud.com>
2025-12-29 22:55:13 +0300

    feat: setup stack navigation

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ padding: 24 }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
---
+++ screens/LoginScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View style={{ padding: 24 }}>
+      <Text>Login Screen</Text>
+    </View>
+  );
+}
